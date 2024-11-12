a469e7f48757cec625dd2bf8aeb4586ab5b4e39f
kshim <kenshirou2401@gmail.com>
2024-11-12 11:56:32 +0900

    MyBatisを実装

--- StudentManagement/src/main/java/raisetech/StudentManagement/Application.java
+++ StudentManagement/src/main/java/raisetech/StudentManagement/Application.java
@@ -1,36 +1,55 @@
 package raisetech.StudentManagement;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-  private String name = "ks";
-  private String age = "37";
+  @Autowired
+  private StudentRepository studentRepository;
+  private StudentCourseRepository studentCourseRepository;
+
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
-  @GetMapping("/studentInfo")
-  public String getStudentInfo() {
-    return name + " " + age + "歳";
+  @GetMapping("/studentList")
+  public List<Student> getStudentList(){
+    return studentRepository.search();
   }
 
-  @PostMapping("/studentInfo")
-  public void setStudentInfo(String name, String age) {
-    this.name = name;
-    this.age = age;
+  @GetMapping("/studentCourseList")
+  public List<StudentCourse> getStudentCourseList(){
+    return studentCourseRepository.search();
   }
 
-  @PostMapping("/studentName")
-  public void updateStudentName(String name) {
-    this.name = name;
-  }
+//  @GetMapping("/student")
+//  public String getStudent(@RequestParam String name) {
+//    Student student = repository.searchByName(name);
+//    return student.getName() + " " + student.getAge() + "歳";
+//  }
+//
+//  @PostMapping("/student")
+//  public void registerStudent(String name, int age) {
+//    repository.registerStudent(name,age);
+//  }
+//
+//  @PatchMapping("/student")
+//  public void updateStudentName(String name,int age) {
+//    repository.updateStudent(name,age);
+//  }
+//
+//  @DeleteMapping("/student")
+//  public void deleteStudent(String name){
+//    repository.deleteStudent(name);
+//  }
 
 }
---
+++ StudentManagement/src/main/java/raisetech/StudentManagement/StudentCourse.java
@@ -0,0 +1,16 @@
+package raisetech.StudentManagement;
+
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StudentCourse {
+  private String id;
+  private String studentId;
+  private String courseName;
+  private LocalDateTime courseStartAt;
+  private LocalDateTime courseEndAt;
+
+}
